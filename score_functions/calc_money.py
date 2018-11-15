e03d7914d994766b3e0e02666d1569964536b9a9
Eveline <43990565+EefPino@users.noreply.github.com>
2018-11-15 16:37:12 +0100

    new

--- score_functions/calc_money.py
+++ score_functions/calc_money.py
@@ -1,6 +1,21 @@
-def calc_money (length, width, surface):
+import sys
+
+# get the path to the coordinates
+sys.path.append(sys.path[0].replace('\\score_functions', '\\coordinates'))
+
+from coordinate import *
+
+def calc_money():
     """
-    This function takes the length of the newly created free standing around the houses
+    This function takes the length of the newly created detachement around the houses
     and calculates the money each house yields.
     """
-    
+    # def __init__(self, y, x):
+    #     self.y = y
+    #     self.x = x
+
+    print(coordinates_large)
+
+if __name__ == "__main__":
+
+    money = calc_money()
