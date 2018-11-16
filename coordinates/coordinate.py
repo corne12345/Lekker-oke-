87c1142790a1960d66cf08a1337f8f50b1d68abe
Corne <39339251+corne12345@users.noreply.github.com>
2018-11-16 09:49:32 +0100

    improving calc function

--- coordinates/coordinate.py
+++ coordinates/coordinate.py
@@ -11,8 +11,8 @@ class Coordinates_little(object):
         coordinates_little = []
         little_coordinates_y = [self.y, self.y, self.y + 8, self.y + 8]
         little_coordinates_x = [self.x, self.x + 8, self.x, self.x + 8]
-        coordinates.append(little_coordinates_y)
-        coordinates.append(little_coordinates_x)
+        coordinates_little.append(little_coordinates_y)
+        coordinates_little.append(little_coordinates_x)
 
         return coordinates_little
 
@@ -29,8 +29,8 @@ class Coordinates_medium(object):
         coordinates_medium = []
         medium_coordinates_y = [self.y, self.y, self.y + 7.5, self.y + 7.5]
         medium_coordinates_x = [self.x, self.x + 10, self.x, self.x + 10]
-        coordinates.append(medium_coordinates_y)
-        coordinates.append(medium_coordinates_x)
+        Coordinates_medium.append(medium_coordinates_y)
+        coordinates_medium.append(medium_coordinates_x)
 
         return coordinates_medium
 
@@ -47,8 +47,8 @@ class Coordinates_large(object):
         coordinates_large = []
         large_coordinates_y = [self.y, self.y, self.y + 10.5, self.y + 10.5]
         large_coordinates_x = [self.x, self.x + 11, self.x, self.x + 11]
-        coordinates.append(large_coordinates_y)
-        coordinates.append(large_coordinates_x)
+        coordinates_large.append(large_coordinates_y)
+        coordinates_large.append(large_coordinates_x)
 
         return coordinates_large
 
