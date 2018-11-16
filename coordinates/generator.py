1a8736c1d957141a832d04252d9fb78c4463d958
CUniversityaccount <44020631+CUniversityaccount@users.noreply.github.com>
2018-11-16 12:46:29 +0100

    Generator update

--- coordinates/generator.py
+++ coordinates/generator.py
@@ -9,10 +9,12 @@ class Coordinates(object):
         list_coordinates = []
         y_x_coordinates = {}
         for _ in range(number):
+            
             # creates a library with the y and x coordinates
             y_x_coordinates.update({"y" : random.randint(0, length),
                                     "x" : random.randint(0, width)})
             list_coordinates.append(y_x_coordinates)
+            y_x_coordinates = {}
         return list_coordinates
 
 if __name__ == "__main__":
