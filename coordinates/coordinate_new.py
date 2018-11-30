1b375d5336d6289599b7d8e4d093449268427d1f
CUniversityaccount <44020631+CUniversityaccount@users.noreply.github.com>
2018-11-30 14:10:11 +0100

    Dry code coordinates

---
+++ coordinates/coordinate_new.py
@@ -0,0 +1,21 @@
+class CoordinatesHouse(object):
+    """
+    Creates coordinates for little houses (one family homes)
+    """
+
+    def __init__(self, y, x, house):
+        self.y = y
+        self.x = x
+        self.house = house
+
+
+    def coordinates(self):
+        coordinates = []
+        coordinates_y = [self.y, self.y, self.y + self.house.length,
+                                self.y + self.house.length]
+        coordinates_x = [self.x, self.x + self.house.width,
+                                self.x, self.x + self.house.width]
+        coordinates.append(coordinates_y)
+        coordinates.append(coordinates_x)
+
+        return coordinates
