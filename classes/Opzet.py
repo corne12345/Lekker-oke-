de84607b2a00068f35a923abf125af314754fb80
EefPino <43990565+EefPino@users.noreply.github.com>
2018-12-12 20:39:39 +0100

    Commit Commends

--- classes/Opzet.py
+++ classes/Opzet.py
@@ -1,9 +1,10 @@
-# the measurements are decimeters (not in foot, filthy emperials)
+# the measurements are in decimeters (not in foot, filthy emperials)
 
-
-
-# Single Home defined
 class LittleHouse(object):
+    """
+    Defines a little house (one-family home).
+    """
+    
     def __init__(self, nr_of_houses, length, width, standard_price, detachement, price_improvement):
         self.detachement = detachement
         self.nr_of_houses = nr_of_houses
@@ -14,14 +15,15 @@ class LittleHouse(object):
         self.number = 0.6 * nr_of_houses
         self.price_improvement = price_improvement
 
-
     def surface_house(self, length, width):
         surface = length * width
         return surface
 
-
-# Bungalow defined
 class MediumHouse(object):
+    """
+    Defines a medium house (bungalow).
+    """
+    
     def __init__(self, nr_of_houses, length, width, standard_price, detachement, price_improvement):
         self.nr_of_houses = nr_of_houses
         self.length = length
@@ -32,8 +34,11 @@ class MediumHouse(object):
         self.detachement = detachement
         self.price_improvement = price_improvement
 
-# Maison defined
 class LargeHouse(object):
+    """
+    Defines a large house (maison).
+    """
+    
     def __init__(self, nr_of_houses, length, width, standard_price, detachement, price_improvement):
         self.nr_of_houses = nr_of_houses
         self.length = length
@@ -45,6 +50,10 @@ class LargeHouse(object):
         self.price_improvement = price_improvement
 
 class Water(object):
+    """
+    Defines the water.
+    """
+        
     def __init__(self, width, length):
         self.width = width
         self.length = length
