020bea7a9657c726b31e2f524662ccfea1f10c8b
Eveline <43990565+EefPino@users.noreply.github.com>
2018-12-16 17:23:24 +0100

    commends, dry

--- coordinates/check.py
+++ coordinates/check.py
@@ -4,16 +4,16 @@ class CheckHouse(object):
     """
     Checks if coordinates of the houses satisfy the conditions.
     """
-    
+
     def __init__(self, grid):
         self.grid = grid
 
     def check_coordinates(self, coordinates, house):
-        corner_points = []        
+        corner_points = []
         house_length = house.length
         house_width = house.width
-        
-        # Checks every coordinate.        
+
+        # Checks every coordinate.
         for place in coordinates:
             point = {'y' : place['y'], 'x': place['x'] }
 
@@ -31,13 +31,15 @@ class CheckHouse(object):
             # Appends the right upper point.
             point['x'] = point['x'] - int(house_width)
             corner_points.append(deepcopy(point))
-            
-            # Appends the left under, upper and right upper points.             
-            for dim in [('x', house_width), ('y', house_length), ('x', - house_width)]:
-                point[dim[0]] = point[dim[0]] + int(dim[1])
-                corner_points.append(deepcopy(point))
 
-            # Checks the points in the grid if there are. already one.
+
+            # # !!!!!!!!!! Nog checken of dit kan !!!!!!!!!!!!!!!!!!!!
+            # # Appends the left under, upper and right upper points.
+            # for dim in [('x', house_width), ('y', house_length), ('x', - house_width)]:
+            #     point[dim[0]] = point[dim[0]] + int(dim[1])
+            #     corner_points.append(deepcopy(point))
+
+            # Checks the points in the grid if there are no houses in its range..
             for xy_coordinate in corner_points:
                 if xy_coordinate["y"] >= 180 or xy_coordinate["y"] < 0 or \
                 xy_coordinate["x"] < 0 or xy_coordinate["x"]  >= 160 or \
@@ -47,6 +49,9 @@ class CheckHouse(object):
                     except:
                         print("Out of range!")
                     place = coordinates.index(place)
+
                     return False, place
+
             corner_points = []
+
         return True, None
