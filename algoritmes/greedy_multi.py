745c0cac269666a2a1badee2325d30994b7e7cab
Eveline <43990565+EefPino@users.noreply.github.com>
2018-12-16 16:53:27 +0100

    dry and commends

--- algoritmes/greedy_multi.py
+++ algoritmes/greedy_multi.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 
 class Greed(object):
     """
-    Takes the grid, calculates the distance between houses and creates houses
+    Takes the grid, calculates the distances between houses and creates houses
     with their coordinates.
     """
 
@@ -21,7 +21,7 @@ class Greed(object):
         self.coordinates = self.create_coordinates()
         self.scores = []
 
-    # creates a list with all the houses name with the correct number
+    # Creates a list with all the houses name with the correct number.
     def create_list_house(self):
         list_houses = []
         for key in self.houses.keys():
@@ -29,61 +29,66 @@ class Greed(object):
 
         return list_houses
 
-    # creates coordinates for the houses
+    # Creates coordinates for the houses.
     def create_coordinates(self):
         """
         Creates the coordinates that need to be calculated and will get the
-        coordinate that gives the highest score
+        coordinate that gives the highest score.
         """
 
+        # Defines multiple variables, lists and dictionaries.
         random_list = self.create_list_house()
         coordinates = []
         valid_set = {}
         original_length = len(random_list)
 
-        # Makes the coordinate and checks if the coordinates are valid
+        # Makes the coordinates and checks if the coordinates are valid.
         while self.number_point < original_length:
             print(self.number_point)
             print(valid_set)
             house = sample(set(random_list), 1)[0]
 
-            # goes through the grid and looks if the detachment is large enough for the y axis
+            # Goes through the grid and looks if the detachment is large enough for the y axis.
             for y_axe in range(0, self.grid.length, self.steps):
                 if y_axe < self.houses[house].detachement or \
                 y_axe > self.grid.length - self.houses[house].detachement:
                     pass
                 else:
 
-                    # Itterate over the x length and controle the detachment in the corners
+                    # Itterates over the x length and controles the detachment in the corners.
                     for x_axe in range(0, self.grid.width, self.steps):
                         if x_axe < self.houses[house].detachement or \
                         x_axe > len(self.grid.grid[y_axe]) - self.houses[house].detachement or \
                         self.grid.grid[y_axe][x_axe] in range(1, 5):
                             pass
 
+                        # Adds the coordinate if it complies the detachment conditions.
                         else:
                             if (x_axe + self.houses[house].width < self.grid.width - self.houses[house].detachement) and \
                             (y_axe + self.houses[house].length < self.grid.length - self.houses[house].detachement):
                                 coordinates.append({"x": x_axe, "y": y_axe })
 
+            # Calculates the score of the coordinates.
             score_coordinate = self.max_score(coordinates, house, valid_set)
             print("score" + str(score_coordinate))
+
             # Will append to the right key or will create the key.
             try:
                 valid_set[house].append(deepcopy(score_coordinate))
             except:
                 valid_set[house] = [deepcopy(score_coordinate)]
 
-
             random_list.pop(random_list.index(house))
             self.number_point += 1
-        return valid_set
 
+        return valid_set
 
     def max_score(self, coordinates, house, valid_set):
         """
-        Calculates the max_score in the grid and returns the max_score
+        Calculates the max_score in the grid and returns the max_score.
         """
+
+        # Defines multiple lists and variables.
         score = []
         self.scores = []
         threads = {}
@@ -93,26 +98,31 @@ class Greed(object):
         max_coordinate = None
         list = []
 
+        # Appends the score if the number point is less than 1.
         if self.number_point < 1:
             for coordinate in coordinates:
                 distance = self.calc_grid_distance(self.calculate_coordinate(coordinate, self.houses[house]))
                 score.append(self.score(distance, self.houses[house]))
 
             index = score.index(max(score))
+
             return coordinates[index]
 
         else:
-            # make 4 different lists
-            half = len(coordinates)//2
+
+            # Makes 4 lists from the coordinates.
+            half = len(coordinates) // 2
 
             half1 = coordinates[:half]
             half2 = coordinates[half:]
-            quarter = len(half1)//2
-            list.append(half1[:quarter])
-            list.append(half1[quarter:])
-            list.append(half2[:quarter])
-            list.append(half2[quarter:])
 
+            quarter = len(half1) // 2
+
+            for quart in [half1, half2]:
+                list_final.append(quart[:quarter])
+                list_final.append(quart[quarter:])
+
+            # !!!!!!!!!!!!! Wat gebeurt hier precies? Nog iets meer commends :) !!!!!!!!!!!!!
             for count in range(len(list)):
                 parent_conn, child_conn = Pipe()
                 receivers.append({"parent" + str(count): parent_conn, "child" + str(count): child_conn })
@@ -134,20 +144,29 @@ class Greed(object):
                 if max_score == None or max_score < score["score"]:
                     max_score = score["score"]
                     max_coordinate = score["coordinate"]
+
         self.score= []
+
         return deepcopy(max_coordinate)
 
     def calc_grid_distance(self, coordinate):
         """
-        Calculates the distance between the coordinate and the border of the grid
+        Calculates the distance between the coordinate and the border of the grid.
         """
+
         right = coordinate["x1"]
         left = self.grid.width - coordinate["x2"]
         south = coordinate["y1"]
         north = self.grid.length - coordinate["y2"]
+
         return min(right, left, south, north)
 
+    # !!!!!!!! Deze calculate_coordinates en score kunnen toch weg? !!!!!!!!!!!
     def calculate_coordinate(self, calculation_point, house):
+        """
+        Describes the corners of an house.
+        """
+
         return {"x1": calculation_point["x"], "x2": calculation_point["x"] + house.width,
                 "y1": calculation_point["y"], "y2": calculation_point["y"] + house.length}
 
@@ -156,14 +175,15 @@ class Greed(object):
         Calculates the score.
         """
 
-        factor = ((distance - house.detachement)* house.price_improvement) + 1
+        factor = ((distance - house.detachement) * house.price_improvement) + 1
         score = int(house.price * factor)
+
         return score
 
 
 class Thread (object):
     """
-    Multithreading and fast calculations
+    Multithreading and fast calculations.
     """
 
     def __init__(self, control_coordinate, houses, key, existing_coordinates, grid):
@@ -176,12 +196,14 @@ class Thread (object):
 
     def calc_grid_distance(self, coordinate):
         """
-        Calculates the distance between the coordinate and the border of the grid
+        Calculates the distance between the coordinate and the border of the grid.
         """
+
         right = coordinate["x1"]
         left = self.grid.width - coordinate["x2"]
         south = coordinate["y1"]
         north = self.grid.length - coordinate["y2"]
+
         return min(right, left, south, north)
 
     def house_in_house (self, valid_set, selected):
@@ -189,12 +211,12 @@ class Thread (object):
         Checks if the coordinates are partially in a house or between a house.
         """
 
-        # Checks if the existing house is in the selected coordinates
+        # Checks if the existing house is in the selected coordinates.
         if (selected["x1"] <= valid_set["x1"] <= selected["x2"] or selected["x1"] <= valid_set["x2"] <= selected["x2"]) and \
         (selected["y1"] <= valid_set["y1"] <= selected["y2"] or selected["y1"] <= valid_set["y2"] <= selected["y2"]):
             return False
 
-        # Checks if the house is in a existing house
+        # Checks if the house is in a existing house.
         elif (valid_set["x1"] <= selected["x1"] <= valid_set["x2"] or valid_set["x1"] <= selected["x2"] <= valid_set["x2"]) and \
         (valid_set["y1"] <= selected["y1"] <= valid_set["y2"] or valid_set["y1"] <= selected["y2"] <= valid_set["y2"]):
             return False
@@ -202,10 +224,9 @@ class Thread (object):
         else:
             return True
 
-
     def controle_function (self, check, existing):
         """
-        Checks if the coordinate is valid on the restrictions given
+        Checks if the coordinate is valid on the given restrictions.
         """
 
         valid_set = check
@@ -217,7 +238,7 @@ class Thread (object):
         if self.house_in_house(valid_set, selected) == False:
             return False
 
-        # Checks if the house is in a vertical or horizontal position of the checkcoordianate
+        # Checks if the house is in a vertical or horizontal position of the checkcoordinate.
         elif valid_set["x1"] < selected["x1"] < valid_set["x2"] or \
         valid_set["x1"] < selected["x2"] < valid_set["x2"]:
             dist = min(abs(valid_set["y1"] - selected["y2"]), abs(selected["y1"] - valid_set["y2"]))
@@ -228,10 +249,11 @@ class Thread (object):
             dist = min(abs(valid_set["x1"] - selected["x2"]), abs(selected["x1"] - valid_set["x2"]))
             return dist
 
-        # Calculates the if the house are diagonal from eachother.
+        # Calculates if the houses are diagonal from eachother.
         else:
 
-            # Checks if the house are left or right from eachother and calculate minimum_distance.
+            # Checks if the houses are left or right from eachother and
+            # calculates the minimum distance.
             if (valid_set["x1"] - selected["x2"]) > 0:
                 x_valid = "x1"
                 x_selected = "x2"
@@ -248,10 +270,18 @@ class Thread (object):
             dist3 = ((valid_set[x_valid] - selected[x_selected]) ** 2 + (valid_set["y2"] - selected["y1"]) **2)**0.5
             dist4 = ((valid_set[x_valid] - selected[x_selected]) ** 2 + (valid_set["y2"] - selected["y2"]) **2)**0.5
 
+            # # !!!!!!!!! Nog checken of dit kan!!!!!!!!!!!!!!!!!!!!
+            # for coord in [("dist1", "y1", "y1"), ("dist2", "y1", "y2"), ("dist3", "y2", "y1"), ("dist4", "y2", "y2")]:
+            #     coord[0] = ((valid_set[x_valid] - selected[xelected]) ** 2 + (valid_set[coord[1]] - selected[coord[2]]) **2)**0.5
+
             dist = min(dist1, dist2, dist3, dist4)
             return dist
 
     def calculate_coordinate(self, calculation_point, house):
+        """
+        Describes the corners of an house.
+        """
+
         return {"x1": calculation_point["x"], "x2": calculation_point["x"] + house.width,
                 "y1": calculation_point["y"], "y2": calculation_point["y"] + house.length}
 
@@ -262,18 +292,25 @@ class Thread (object):
 
         factor = ((distance - house.detachement)* house.price_improvement) + 1
         score = int(house.price * factor)
+
         return score
 
     def run(self, conn):
+        """
+        gh???!!!!
+        """
+
         score = []
         max_score = None
         minimum_distance = None
 
+        # Itterates over all coordinates.
         for coordinate in self.coordinates:
             minimum_distance = None
             control = True
             selected = self.calculate_coordinate(coordinate , self.houses[self.key])
 
+            # Checks if the coordinates are within the conditions.
             for key, check in self.existing_coordinates.items():
                 for check_coordinate in check:
                     valid = self.calculate_coordinate(check_coordinate, self.houses[key])
@@ -287,7 +324,7 @@ class Thread (object):
                         control = False
                         break
 
-                    # checks if the distance bigger then the detachement
+                    # Checks if the distance is larger than the detachement.
                     if dist < max(self.houses[key].detachement, self.houses[self.key].detachement):
                         control = False
                         break
@@ -307,10 +344,11 @@ class Thread (object):
                 if grid_space < self.houses[self.key].detachement:
                     score.append(0)
                     break
-                    
+
                 else:
                     score.append(self.score(minimum_distance, self.houses[self.key]))
 
+        # Defines the score.
         index = score.index(max(score))
         max_score = {"score": score[index], "coordinate": self.coordinates[index]}
         conn.send(max_score)
