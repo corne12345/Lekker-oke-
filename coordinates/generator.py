45f64935f2d7d71d34aafebb32bd68a981f0c463
Eveline <43990565+EefPino@users.noreply.github.com>
2018-12-16 17:30:09 +0100

    little change

--- coordinates/generator.py
+++ coordinates/generator.py
@@ -1,37 +1,51 @@
 import random
 
 class Coordinates(object):
+    """
+    Generates coordinates for houses.
+    """
+
     def __init__(self, number, length, width, grid):
         self.length = length
         self.width = width
         self.grid = grid
         self.coordinates = self.create_coordinates(number)
 
-    # random generator
     def create_coordinates(self, number):
+        """
+        Generates random coordinates.
+        """
+
         list_coordinates = []
         y_x_coordinates = {}
+
+        # Iterates trough the number.
         for _ in range(number):
 
-            # creates a library with the y and x coordinates
+            # Creates a library with the y and x coordinates.
             y_x_coordinates.update({"y" : random.randint(0, self.length),
                                     "x" : random.randint(0, self.width)})
 
-            # check if water is on the place 
+            # Checks if water is on the place.
             while self.length <= y_x_coordinates["y"] \
             or self.width <= y_x_coordinates["x"] \
             or self.grid[y_x_coordinates["y"]][y_x_coordinates["x"]] == 1 :
                 y_x_coordinates = ({"y" : random.randint(0, self.length),
                                     "x" : random.randint(0, self.width)})
 
-
             list_coordinates.append(y_x_coordinates)
             y_x_coordinates = {}
+
         return list_coordinates
 
     def single_coordinate(self):
+        """
+        Appends a random coordinate.
+        """
+
         y_x_coordinates = ({"y" : random.randint(0, self.length),
                             "x" : random.randint(0, self.width)})
+
         return y_x_coordinates
 
 
