1453bc3b332479c949eeeb31b7fc2058089c2bd0
Eveline <43990565+EefPino@users.noreply.github.com>
2018-12-16 18:34:45 +0100

    comment changes

--- grid/grid_houses.py
+++ grid/grid_houses.py
@@ -23,6 +23,10 @@ from hillclimber_corne import *
 # 4 = large house (maison)
 # 5 = detachement
 class Grid(object):
+    """
+    Defines the grid.
+    """
+
     def __init__(self, total_length, total_width):
         self.width = total_width
         self.length = total_length
@@ -45,38 +49,44 @@ class Grid(object):
                 list.append(text)
         print(list)
 
-
-    # makes the grid itself
     def grid_command(self):
+        """
+        Creates the grid itself.
+        """
+
         grid = []
         grid_row = []
+
         for length in range(self.length):
             for pixel in range(self.width):
                 grid_row.append(0)
             grid.append(grid_row)
             grid_row = []
+
         return grid
 
-    # creates different waterbodies
     def create_water(self, file):
         """
         Creates different waterbodies
         """
+
         water = file
         first_length_position = None
         first_width_position = None
         last_width_position = None
 
-        # creates the water and checks if there is nothing there
-        for row in range(len(self.grid)): # length
+        # Creates the water and checks if there is nothing there.
+        for row in range(len(self.grid)):
 
-            # get the first position of the length
+            # Get the first position of the length.
             if first_length_position == None:
                 first_length_position = row
 
             if water.length > (row - first_length_position) \
             and first_length_position != None:
-                for place, item in enumerate(self.grid[0]): # width
+
+                # Checks the width.
+                for place, item in enumerate(self.grid[0]):
                     if item == 0 and first_width_position == None:
                         first_width_position = place
                         self.grid[row][place] = 1
@@ -85,45 +95,47 @@ class Grid(object):
                     and water.width > (place - first_width_position):
                         self.grid[row][place] = 1
 
-        # calculates the surface
+        # Calculates the surface.
         surface = water.length * water.width
         self.waterbody = {"y": first_width_position, "x": first_width_position}
 
 
     def create_house(self, little, middle, large, water):
         """
-        Creates homes
+        Creates houses.
         """
-        # self.coordinates = Check(self.grid).check()
-        # coordinates = self.coordinates
+
+        # self.coordinates = Check(self.grid).check() !!!!!!!!!
+        # coordinates = self.coordinates !!!!!!!!!!!!!!!!!
         houses ={"little": little, "medium": middle, "large": large}
 
-        # Code necessary to visualise random function
+        # Code necessary to visualise the random function.
         output = random_to_vis(hillclimber(1, 3, 1))
         coordinates, max_score, max_distances  = output[0], output[1], output[2]
         self.coordinates = coordinates
-        # print(coordinates, max_score, max_distances, sep='\n')
+        # print(coordinates, max_score, max_distances, sep='\n') !!!!!!!
 
         first_length_position = None
         first_width_position = None
 
+        # Iterates trough all the coordinates of the houses.
         for house in coordinates.keys():
             for number in range(len(coordinates[house])):
 
-                # coordinates, navragen of library beter is
+                # coordinates, navragen of library beter is !!!!!!!!
                 coordinate = coordinates[house][number]
 
-                # import the grid and put the houses in the right spaces
-                for row in range(len(self.grid)): # to do iterate  over the grid to put the houses on the right places
-                    if first_length_position == None and row == round(coordinate["y"]): # nog nakijken of de huizen goed positioneerd
+                # Imports the grid and puts the houses at the right places.
+                for row in range(len(self.grid)): # to do iterate  over the grid to put the houses on the right places !!!!!!!!!!!!!!!
+                    if first_length_position == None and row == round(coordinate["y"]): # nog nakijken of de huizen goed positioneerd !!!!!!!!!!!!!!
                         first_length_position = row
 
-                    # get first x position
+                    # Get first x position.
                     if first_length_position != None and \
                     first_length_position + houses[house].length > row :
                         for place in range(len(self.grid[0])):
 
-                            # fill the first the x position
+                            # Fill the first x position.
                             if self.grid[row][place] not in range(1, 5) and first_width_position == None and place == round(coordinate["x"]):
                                 first_width_position = place
                                 if house == "little":
@@ -133,7 +145,7 @@ class Grid(object):
                                 else:
                                     self.grid[row][place] = 4
 
-                            # fill the grid in
+                            # Fill the grid in with houses.
                             elif first_width_position != None and self.grid[row][place] not in range(1, 5) and \
                             first_width_position + houses[house].width > place and place - first_width_position >= 0:
                                 if house == "little":
@@ -143,9 +155,11 @@ class Grid(object):
                                 else:
                                     self.grid[row][place] = 4
 
-
-# Visualizes the graph
 class Visualator(object):
+    """
+    Visualizes the grid with houses and water.
+    """
+
     def __init__(self, grid, little_house, medium_house, large_house, water, coordinates):
         self.grid = grid.grid
         self.coordinates = coordinates
@@ -155,30 +169,33 @@ class Visualator(object):
     def bokeh(self):
         graph = figure(title = "Amstelhaege")
 
-        # get x values for the bokeh figure
+        # for dim in [x, y]:
+        #     dim_axis
+        #
+        # Get x values for the bokeh figure.
         x_axis = self.grid[-1]
         first_x = x_axis.index(x_axis[0])
         last_x = None
         for count, values in enumerate(x_axis):
             last_x = count + 1
 
-        # get y values for graph
+        # Get y values for graph.
         y_axis = self.grid
         first_y = y_axis.index(y_axis[0])
         last_y = None
         for count, values in enumerate(y_axis):
             last_y = count + 1
 
-        # makes the  graph and changes the aesthetics if the graph
+        # Makes the graph and changes the aesthetics if the graph.
         graph.x_range = Range1d(last_x, first_x)
         graph.y_range = Range1d(last_y, first_y)
 
-        # makes ground plan polygon
+        # Makes a ground plan polygon.
         graph.patch(x=[first_x, first_x, last_x, last_x],
                     y=[first_y, last_y, last_y, first_y],
                     color="grey")
 
-        # make waterbody
+        # Makes a waterbody.
         graph.patch(x=[self.waterbody["x"], self.waterbody["x"],
                        self.waterbody["x"] + self.water.width,
                        self.waterbody["x"] + self.water.width],
@@ -188,7 +205,7 @@ class Visualator(object):
                        self.waterbody["y"]],
                     color="blue", line_color="black")
 
-        # makes the houses into the graph
+        # Puts the houses into the graph.
         for sort in self.houses.keys():
             colour = None
 
--- grid/grid_houses_copy.py
+++ grid/grid_houses_copy.py
@@ -14,11 +14,15 @@ from calc_money import *
 
 
 # 1 = waterbody
-# 2 = little house (single family home)
+# 2 = little house (single-family home)
 # 3 = medium house (bungalow)
 # 4 = large house (maison)
 # 5 = detachement
 class Grid(object):
+    """
+    Defines the grid.
+    """
+
     def __init__(self, total_length, total_width):
         self.width = total_width
         self.length = total_length
@@ -40,19 +44,22 @@ class Grid(object):
                 list.append(text)
         print(list)
 
-
-    # makes the grid itself
     def grid_command(self):
+        """
+        Creates the grid itself.
+        """
+
         grid = []
         grid_row = []
+
         for length in range(self.length):
             for pixel in range(self.width):
                 grid_row.append(0)
             grid.append(grid_row)
             grid_row = []
+
         return grid
 
-    # creates different waterbodies
     def create_water(self, file):
         """
         Creates different waterbodies.
@@ -67,22 +74,23 @@ class Grid(object):
         rows = []
         places = []
 
-        # creates the water and checks if there is nothing there
-        for row in range(len(self.grid)): # length
+        # Creates the water and checks if there is nothing there.
+        for row in range(len(self.grid)):
 
-            # get the first position of the length
+            # Get the first position of the length to check.
             if first_length_position == None:
                 first_length_position = row
 
             if water.length > (row - first_length_position) \
             and first_length_position != None:
-                for place, item in enumerate(self.grid[0]): # width
+
+                # Checks the width.
+                for place, item in enumerate(self.grid[0]):
                     if item == 0 and first_width_position == None:
                         first_width_position = place
                         self.grid[row][place] = 1
                         rows.append(row)
                         places.append(place)
-
 
                     elif first_width_position != None and place >= first_width_position \
                     and water.width > (place - first_width_position):
@@ -91,48 +99,52 @@ class Grid(object):
                         rows.append(row)
                         places.append(place)
 
-        # makes a list with the x and y coordinates were water is
+        # Makes a list with the x and y coordinates of the water.
         waterlist.append(rows)
         waterlist.append(places)
 
-        # calculates the surface
+        # Calculates the surface.
         surface = water.length * water.width
         return surface, waterlist
 
 
     def create_little_house(self, file):
         """
-        Creates little homes (single family homes).
+        Creates little homes (single-family homes).
         """
 
         little_house = file
         coordinates = self.coordinates["little"]
 
+        # Defines the coordinates for the houses.
         for number in range(len(coordinates)):
 
-            # coordinates, navragen of library beter is
+            # coordinates, navragen of library beter is !!!!!!!!!!
             coordinate = coordinates[number]
             y_axis = round(coordinate["y"])
             x_axis = round(coordinate["x"])
             first_length_position = None
             first_width_position = None
 
-            # import the grid and put the houses in the right spaces
-            for row in range(len(self.grid)): # to do iterate over the grid to put the houses on the right places
-                if first_length_position == None and row == y_axis : # nog nakijken of de huizen goed positioneerd
+            # Import the grid and put the houses in the right spaces.
+            for row in range(len(self.grid)): # to do iterate over the grid to put the houses on the right places !!!!!!
+                if first_length_position == None and row == y_axis : # nog nakijken of de huizen goed positioneerd !!!!!!!!!!!
                     first_length_position = row
 
+                # Get first x position.
                 if first_length_position != None and \
                 first_length_position + little_house.length > row :
                     for place in range(len(self.grid[0])):
+
+                        # Fill the first x position.
                         if self.grid[row][place] not in range(1, 5) and first_width_position == None and place == x_axis:
                             first_width_position = place
                             self.grid[row][place] = 2
 
+                        # Fill the grid in with houses.
                         elif first_width_position != None and self.grid[row][place] not in range(1, 5) and \
                         first_width_position + little_house.width > place and place - first_width_position >= 0:
                             self.grid[row][place] = 2
-
 
     def create_medium_house(self, file):
         """
@@ -143,6 +155,7 @@ class Grid(object):
         coordinates = self.coordinates["medium"]
         print(coordinates)
 
+        # Defines the coordinates for the houses.
         for number in range(len(coordinates)):
 
             coordinate = coordinates[number]
@@ -151,17 +164,22 @@ class Grid(object):
             first_length_position = None
             first_width_position = None
 
-            # import the grid and put the houses in the right spaces
-            for row in range(len(self.grid)): # to do iterate over the grid to put the houses on the right places
-                if first_length_position == None and row == y_axis : # nog nakijken of de huizen goed positioneerd
+            # Imports the grid and puts the houses in the right spaces.
+            for row in range(len(self.grid)): # to do iterate over the grid to put the houses on the right places !!!!!!
+                if first_length_position == None and row == y_axis : # nog nakijken of de huizen goed positioneerd !!!!!!!!
                     first_length_position = row
 
+                # Get first x position.
                 if first_length_position != None and \
                 first_length_position + medium_house.length > row :
                     for place in range(len(self.grid[0])):
+
+                        # Fill the first x position.
                         if self.grid[row][place] not in range(1, 5) and first_width_position == None and place == x_axis:
                             first_width_position = place
                             self.grid[row][place] = 3
+
+                        # Fill the grid in with houses.
                         elif first_width_position != None and self.grid[row][place] not in range(1, 5) and \
                         first_width_position + medium_house.width > place and place - first_width_position >= 0:
                             self.grid[row][place] = 3
@@ -175,33 +193,41 @@ class Grid(object):
         large_house = file
         coordinates = self.coordinates["large"]
 
+        # Defines the coordinates for the houses.
         for number in range(len(coordinates)):
 
-            # coordinates, navragen of library beter is
+            # coordinates, navragen of library beter is !!!!!!!!
             coordinate = coordinates[number]
             y_axis = round(coordinate["y"])
             x_axis = round(coordinate["x"])
             first_length_position = None
             first_width_position = None
 
-            # import the grid and put the houses in the right spaces
-            for row in range(len(self.grid)): # to do iterate over the grid to put the houses on the right places
-                if first_length_position == None and row == y_axis : # nog nakijken of de huizen goed positioneerd
+            # Import the grid and put the houses in the right spaces.
+            for row in range(len(self.grid)): # to do iterate over the grid to put the houses on the right places !!!!!!!!
+                if first_length_position == None and row == y_axis : # nog nakijken of de huizen goed positioneerd !!!!!!!!!
                     first_length_position = row
 
+                # Get first x position.
                 if first_length_position != None and \
                 first_length_position + large_house.length > row :
                     for place in range(len(self.grid[0])):
+
+                        # Fill the first x position.
                         if self.grid[row][place] not in range(1, 5) and first_width_position == None and place == x_axis:
                             first_width_position = place
                             self.grid[row][place] = 4
+
+                        # Fill the grid in with houses.
                         elif first_width_position != None and self.grid[row][place] not in range(1, 5) and \
                         first_width_position + large_house.width > place and place - first_width_position >= 0:
                             self.grid[row][place] = 4
 
-
-# Visualizes the graph
 class Visualator(object):
+    """
+    Visualizes the grid with houses and water.
+    """
+
     def __init__(self, grid, little_house, medium_house, large_house):
         self.grid = grid
         self.little_house = little_house
@@ -211,35 +237,35 @@ class Visualator(object):
     def bokeh(self):
         graph = figure(title = "Amstelhaege")
 
-        # get x values for the bokeh figure
+        # Get x values for the bokeh figure.
         x_axis = self.grid[-1]
         first_x = x_axis.index(x_axis[0])
         last_x = None
         for count, values in enumerate(x_axis):
             last_x = count + 1
 
-        # get y values for graph
+        # Get y values for graph.
         y_axis = self.grid
         first_y = y_axis.index(y_axis[0])
         last_y = None
         for count, values in enumerate(y_axis):
             last_y = count + 1
 
-        # makes the  graph and changes the aesthetics if the graph
+        # Makes the graph and changes the aesthetics if the graph.
         graph.x_range = Range1d(last_x, first_x)
         graph.y_range = Range1d(last_y, first_y)
 
-        # makes a waterbody and datapoints
+        # Makes a waterbody and datapoints.
         water_first_x = None
         water_first_y = None
         water_last_x = None
         water_last_y = None
 
-        # makes the datapoints
+        # Makes the datapoints.
         for y, list in enumerate(self.grid):
 
-            # misschien dubble loepen
-            # make water body
+            # misschien dubble loepen !!!!!!!!!!!!!!!!
+            # Makes a waterbody.
             if 1 in list and water_first_x == None:
                 water_first_x = list[::1].index(1)
                 water_first_y = y + 1
@@ -247,19 +273,20 @@ class Visualator(object):
                 water_last_x = len(list) - list[::-1].index(1)
                 water_last_y = y + 1
 
-        # checks if water exist and print the ground
+        # Checks if water exist and prints the ground.
         if water_first_x != None:
 
-            # makes ground plan polygon
+            # Makes a ground plan polygon.
             graph.patch(x=[first_x, first_x, last_x, last_x],
                         y=[first_y, last_y, last_y, first_y],
                         color="grey")
 
-            # makes water polygon
+            # Makes a water polygon.
             graph.patch(x=[water_first_x, water_first_x, water_last_x, water_last_x],
                         y=[water_first_y, water_last_y, water_last_y, water_first_y],
                         color="blue", line_color="black")
 
+        # Puts the houses into the graph.
         for sort in grid.coordinates.keys():
             color = None
 
