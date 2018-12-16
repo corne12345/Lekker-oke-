9903313cca0d865e6ba92d501e7d837ac7c36462
Eveline <43990565+EefPino@users.noreply.github.com>
2018-12-16 21:46:17 +0100

    little changes

--- visualisation/graphs.py
+++ visualisation/graphs.py
@@ -3,33 +3,41 @@ from bokeh.io import export_png
 from bokeh.plotting import figure, output_file, show
 
 class Graph(object):
+    """
+    Gives the data of a csv.
+    """
+
     def __init__(self):
         self.bokeh(self.load_csv("visualisation_hill.csv"))
 
     def load_csv(self, data):
-        itteration = []
+        iteration = []
         value = []
+
+        # Reads the values in the data.
         with open(data, "r") as data:
             reader = csv.reader(data)
             for line in reader:
                 line = line[0].split(";")
                 try:
                     value.append(int(line[0]))
-                    itteration.append(int(line[1]))
+                    iteration.append(int(line[1]))
                 except:
                     print("No number")
 
-        return itteration, value
+        return iteration, value
 
     def bokeh(self, data):
-        itteration, value = data
+        """
+        Makes a bokeh plot from the data.
+        """
+
+        iteration, value = data
         graph = figure(plot_width=1600, plot_height=1600)
 
-        graph.line(value, itteration, line_width=2)
+        graph.line(value, iteration, line_width=2)
 
         export_png(graph, filename="hillclimber.png")
-
-
 
 if __name__ == "__main__":
     Graph()
