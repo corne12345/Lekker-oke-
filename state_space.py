698cd523c41c06d635d45914155d6d509eafb2ee
Eveline <43990565+EefPino@users.noreply.github.com>
2018-12-17 11:17:03 +0100

    change

--- state_space.py
+++ state_space.py
@@ -1,30 +1,34 @@
-""""
-This program calculates the state space of the Amstelhaege case for a set amount of houses.
-"""
+def main():
+    """"    
+    This program calculates the state space of the Amstelhaege case for a set amount of houses.
+    """
 
-AMOUNT_HOUSES = 60
-SMALL_HOUSES = int(0.6 * AMOUNT_HOUSES)
-MEDIUM_HOUSES = int(0.25 * AMOUNT_HOUSES)
-LARGE_HOUSES = int(0.15 * AMOUNT_HOUSES)
+    AMOUNT_HOUSES = 60
+    SMALL_HOUSES = int(0.6 * AMOUNT_HOUSES)
+    MEDIUM_HOUSES = int(0.25 * AMOUNT_HOUSES)
+    LARGE_HOUSES = int(0.15 * AMOUNT_HOUSES)
 
-area_small = 64
-options = 1
+    area_small = 64
+    options = 1
 
-# Calculates the amount of options for each house
-for i in range(SMALL_HOUSES):
-    options_temp = (24864 - area_small * i)
-    options *= options_temp
+    # Calculates the amount of options for each house
+    for i in range(SMALL_HOUSES):
+        options_temp = (24864 - area_small * i)
+        options *= options_temp
 
-area_medium = 75
-for i in range(MEDIUM_HOUSES):
-    opp = 144 * 166.5 - SMALL_HOUSES * area_small
-    options_temp = opp - i * area_medium
-    options *= options_temp
+    area_medium = 75
+    for i in range(MEDIUM_HOUSES):
+        opp = 144 * 166.5 - SMALL_HOUSES * area_small
+        options_temp = opp - i * area_medium
+        options *= options_temp
 
-area_large = 115.5
-for i in range(LARGE_HOUSES):
-    opp = 137 * 157.5 - SMALL_HOUSES * area_small - MEDIUM_HOUSES * area_medium
-    options_temp = opp - i * area_large
-    options *= options_temp
+    area_large = 115.5
+    for i in range(LARGE_HOUSES):
+        opp = 137 * 157.5 - SMALL_HOUSES * area_small - MEDIUM_HOUSES * area_medium
+        options_temp = opp - i * area_large
+        options *= options_temp
 
-print(options)
+    print(options)
+
+if __name__ == '__main__':
+    main()
